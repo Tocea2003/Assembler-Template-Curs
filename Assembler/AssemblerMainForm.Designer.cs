4a4e6535e5d76db82ee345c0f0d1fba76361209e
Lavu Cristian <cristianioan.lavu@ulbsibiu.ro>
2025-03-20 12:06:18 +0200

    Add project files.

---
+++ Assembler/AssemblerMainForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Assembler
+{
+    partial class AssemblerMainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ParseFileButton = new System.Windows.Forms.Button();
+            this.ASMFileLabel = new System.Windows.Forms.Label();
+            this.ASMFileTextBox = new System.Windows.Forms.TextBox();
+            this.OpenFileButton = new System.Windows.Forms.Button();
+            this.OutputTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ParseFileButton
+            // 
+            this.ParseFileButton.Enabled = false;
+            this.ParseFileButton.Location = new System.Drawing.Point(25, 106);
+            this.ParseFileButton.Name = "ParseFileButton";
+            this.ParseFileButton.Size = new System.Drawing.Size(118, 38);
+            this.ParseFileButton.TabIndex = 0;
+            this.ParseFileButton.Text = "Parse ASM file";
+            this.ParseFileButton.UseVisualStyleBackColor = true;
+            this.ParseFileButton.Click += new System.EventHandler(this.ParseFileButton_Click);
+            // 
+            // ASMFileLabel
+            // 
+            this.ASMFileLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.ASMFileLabel.AutoSize = true;
+            this.ASMFileLabel.Location = new System.Drawing.Point(22, 24);
+            this.ASMFileLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.ASMFileLabel.Name = "ASMFileLabel";
+            this.ASMFileLabel.Size = new System.Drawing.Size(98, 17);
+            this.ASMFileLabel.TabIndex = 31;
+            this.ASMFileLabel.Text = "ASM file name";
+            // 
+            // ASMFileTextBox
+            // 
+            this.ASMFileTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.ASMFileTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ASMFileTextBox.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.ASMFileTextBox.Location = new System.Drawing.Point(25, 45);
+            this.ASMFileTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.ASMFileTextBox.Name = "ASMFileTextBox";
+            this.ASMFileTextBox.Size = new System.Drawing.Size(494, 22);
+            this.ASMFileTextBox.TabIndex = 29;
+            this.ASMFileTextBox.Text = "Select the file which contains the asm code";
+            // 
+            // OpenFileButton
+            // 
+            this.OpenFileButton.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.OpenFileButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OpenFileButton.Location = new System.Drawing.Point(536, 45);
+            this.OpenFileButton.Margin = new System.Windows.Forms.Padding(4);
+            this.OpenFileButton.Name = "OpenFileButton";
+            this.OpenFileButton.Size = new System.Drawing.Size(31, 28);
+            this.OpenFileButton.TabIndex = 30;
+            this.OpenFileButton.Text = "...";
+            this.OpenFileButton.UseVisualStyleBackColor = true;
+            this.OpenFileButton.Click += new System.EventHandler(this.OpenFileButton_Click);
+            // 
+            // OutputTextBox
+            // 
+            this.OutputTextBox.Location = new System.Drawing.Point(25, 171);
+            this.OutputTextBox.Multiline = true;
+            this.OutputTextBox.Name = "OutputTextBox";
+            this.OutputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.OutputTextBox.Size = new System.Drawing.Size(542, 265);
+            this.OutputTextBox.TabIndex = 32;
+            // 
+            // AssemblerMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 533);
+            this.Controls.Add(this.OutputTextBox);
+            this.Controls.Add(this.ASMFileLabel);
+            this.Controls.Add(this.ASMFileTextBox);
+            this.Controls.Add(this.OpenFileButton);
+            this.Controls.Add(this.ParseFileButton);
+            this.Name = "AssemblerMainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Assembler for didactical processor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ParseFileButton;
+        private System.Windows.Forms.Label ASMFileLabel;
+        private System.Windows.Forms.TextBox ASMFileTextBox;
+        private System.Windows.Forms.Button OpenFileButton;
+        private System.Windows.Forms.TextBox OutputTextBox;
+    }
+}
+
---
+++ Assembler/AssemblerMainForm.cs
@@ -0,0 +1,159 @@
+﻿/***********************************************************************************************
+ * Project: Simulator of an microprogrammed Didactical Processor (DP)
+ * Module: Assembler for DP
+ * Filename: AssemblerMainForm.cs
+ * Users: Students in Computer Science (3rd year of study)
+ * Creator: Horia V. Caprita
+ * Date: 10.03.2015 
+***********************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+
+
+namespace Assembler
+{
+    public partial class AssemblerMainForm : Form
+    {
+        private String filename;
+
+        public AssemblerMainForm()
+        {
+            InitializeComponent();
+        }
+
+        /* Function used to obtain the ASM filename (*.asm) */
+        private String getFileName(String filter)
+        {
+            try
+            {
+                /* Local variable used to store the filename */
+                String fileNameWithPath = "";
+                /* Instantiate an OpenFileDialog */
+                OpenFileDialog of = new OpenFileDialog();
+                /* Set the filter */
+                of.Filter = filter;
+                /* Get the working directory */
+                of.InitialDirectory = Path.GetFullPath("..\\Debug");
+                of.RestoreDirectory = true;
+                /* Display the Open File dialog */
+                if (of.ShowDialog() == DialogResult.OK)
+                {
+                    /* Get only the filename with full path */
+                    fileNameWithPath = of.FileName;
+                    /* Get only the filename without path */
+                    filename = of.SafeFileName;
+                }
+                /* Return the filename with complete path */
+                return fileNameWithPath;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return null;
+            }
+        }
+
+        private void ParseFileButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                /* local variable used for debugging only */
+                int lineCounter = 0;
+                /* List which will store each token (element) read from ASM file */
+                List<String> asmElements = new List<String>();
+                /* Create a parser object used for ASM file
+                    REMEMBER: this parser can be used for all kind of text files!!!
+                 */
+                TextFieldParser parser = new TextFieldParser(filename);
+                /* Reinitialize the Text property of OutputTextBox */
+                OutputTextBox.Text = "";
+                /* Define delimiters in ASM file */
+                String[] delimiters = {":",","," ","(",")"};
+                /* Specify that the elements in ASM file are delimited by some characters */
+                parser.TextFieldType = FieldType.Delimited;
+                /* Set-up the specified delimiters */
+                parser.SetDelimiters(delimiters);
+                /* Parse the entire ASM file based on previous specifications*/
+                while (!parser.EndOfData)
+                {
+                    /* Read an entire line in ASM file
+                       and split this line in many strings delimited by delimiters */
+                    string[] asmFields = parser.ReadFields();
+                    /* Store each string as a single element in the list
+                       if this string is not empty */
+                    foreach (string s in asmFields)
+                    {
+                        if (!s.Equals(""))
+                        {
+                            asmElements.Add(s);
+                        }
+                    }
+                    /* Counting the number of lines stored in ASM file */
+                    lineCounter++;
+                }
+
+                /* Close the parser */
+                parser.Close();
+                /* If the file is empty, trigger a new exception which
+                   in turn will display an error message */
+                if (lineCounter == 0)
+                {
+                    Exception exc = new Exception("The ASM file is empty!");
+                    throw exc;
+                }
+                else
+                {
+                    /* Display every token in OutputTextBox */
+                    foreach (String s in asmElements)
+                    {
+                        OutputTextBox.Text += s + Environment.NewLine;
+                    }
+                    /* Display an information about the process completion */
+                    MessageBox.Show("Parsing is completed!", "Assembler information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void OpenFileButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                /* String used to be displayed in ASMFileTextBox */
+                String filename = "";
+                /* Reinitialize the Text property of OutputTextBox */
+                OutputTextBox.Text = "";
+                /* Take the filename selected by user */
+                filename = getFileName("ASM file for didactical processor(*.asm)|*.asm");
+                /* Display the filename in ASMFileTextBox */
+                ASMFileTextBox.Text = filename != null ? filename : ASMFileTextBox.Text;
+                /* Enable/Disable the ParseFileButton depending of user choice */
+                if (!filename.Equals(""))
+                {
+                    ParseFileButton.Enabled = true;
+                }
+                else
+                {
+                    ParseFileButton.Enabled = false;
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
