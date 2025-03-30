059df11720f33776ff397acd279c33304cd9eb5c
Lavu Cristian <cristianioan.lavu@ulbsibiu.ro>
2025-03-30 16:11:41 +0300

    Part 1

--- Assembler/AssemblerMainForm.Designer.cs
+++ Assembler/AssemblerMainForm.Designer.cs
@@ -33,14 +33,16 @@
             this.ASMFileTextBox = new System.Windows.Forms.TextBox();
             this.OpenFileButton = new System.Windows.Forms.Button();
             this.OutputTextBox = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // ParseFileButton
             // 
             this.ParseFileButton.Enabled = false;
-            this.ParseFileButton.Location = new System.Drawing.Point(25, 106);
+            this.ParseFileButton.Location = new System.Drawing.Point(19, 86);
+            this.ParseFileButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ParseFileButton.Name = "ParseFileButton";
-            this.ParseFileButton.Size = new System.Drawing.Size(118, 38);
+            this.ParseFileButton.Size = new System.Drawing.Size(88, 31);
             this.ParseFileButton.TabIndex = 0;
             this.ParseFileButton.Text = "Parse ASM file";
             this.ParseFileButton.UseVisualStyleBackColor = true;
@@ -50,10 +52,9 @@
             // 
             this.ASMFileLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.ASMFileLabel.AutoSize = true;
-            this.ASMFileLabel.Location = new System.Drawing.Point(22, 24);
-            this.ASMFileLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.ASMFileLabel.Location = new System.Drawing.Point(16, 30);
             this.ASMFileLabel.Name = "ASMFileLabel";
-            this.ASMFileLabel.Size = new System.Drawing.Size(98, 17);
+            this.ASMFileLabel.Size = new System.Drawing.Size(75, 13);
             this.ASMFileLabel.TabIndex = 31;
             this.ASMFileLabel.Text = "ASM file name";
             // 
@@ -62,10 +63,9 @@
             this.ASMFileTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.ASMFileTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ASMFileTextBox.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.ASMFileTextBox.Location = new System.Drawing.Point(25, 45);
-            this.ASMFileTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.ASMFileTextBox.Location = new System.Drawing.Point(19, 47);
             this.ASMFileTextBox.Name = "ASMFileTextBox";
-            this.ASMFileTextBox.Size = new System.Drawing.Size(494, 22);
+            this.ASMFileTextBox.Size = new System.Drawing.Size(330, 19);
             this.ASMFileTextBox.TabIndex = 29;
             this.ASMFileTextBox.Text = "Select the file which contains the asm code";
             // 
@@ -73,10 +73,9 @@
             // 
             this.OpenFileButton.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.OpenFileButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.OpenFileButton.Location = new System.Drawing.Point(536, 45);
-            this.OpenFileButton.Margin = new System.Windows.Forms.Padding(4);
+            this.OpenFileButton.Location = new System.Drawing.Point(404, 47);
             this.OpenFileButton.Name = "OpenFileButton";
-            this.OpenFileButton.Size = new System.Drawing.Size(31, 28);
+            this.OpenFileButton.Size = new System.Drawing.Size(23, 23);
             this.OpenFileButton.TabIndex = 30;
             this.OpenFileButton.Text = "...";
             this.OpenFileButton.UseVisualStyleBackColor = true;
@@ -84,26 +83,41 @@
             // 
             // OutputTextBox
             // 
-            this.OutputTextBox.Location = new System.Drawing.Point(25, 171);
+            this.OutputTextBox.Location = new System.Drawing.Point(19, 139);
+            this.OutputTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.OutputTextBox.Multiline = true;
             this.OutputTextBox.Name = "OutputTextBox";
             this.OutputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.OutputTextBox.Size = new System.Drawing.Size(542, 265);
+            this.OutputTextBox.Size = new System.Drawing.Size(408, 216);
             this.OutputTextBox.TabIndex = 32;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(466, 139);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(381, 216);
+            this.textBox1.TabIndex = 33;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // AssemblerMainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(588, 533);
+            this.ClientSize = new System.Drawing.Size(895, 452);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.OutputTextBox);
             this.Controls.Add(this.ASMFileLabel);
             this.Controls.Add(this.ASMFileTextBox);
             this.Controls.Add(this.OpenFileButton);
             this.Controls.Add(this.ParseFileButton);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "AssemblerMainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Assembler for didactical processor";
+            this.Load += new System.EventHandler(this.AssemblerMainForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -116,6 +130,7 @@
         private System.Windows.Forms.TextBox ASMFileTextBox;
         private System.Windows.Forms.Button OpenFileButton;
         private System.Windows.Forms.TextBox OutputTextBox;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Assembler/AssemblerMainForm.cs
+++ Assembler/AssemblerMainForm.cs
@@ -19,10 +19,72 @@ using System.IO;
 using Microsoft.VisualBasic.FileIO;
 
 
+
+
 namespace Assembler
 {
+
+
+    
+
+
     public partial class AssemblerMainForm : Form
     {
+
+
+        private Dictionary<string, string> opcodeTable = new Dictionary<string, string>
+{
+    {"MOV", "0000"},
+    {"ADD", "0001"},
+    {"SUB", "0010"},
+    {"CMP", "0011"},
+    {"AND", "0100"},
+    {"OR", "0101"},
+    {"XOR", "0110"},
+    {"CLR", "1000000000"},
+    {"NEG", "1000000001"},
+    {"INC", "1000000010"},
+    {"DEC", "1000000011"},
+    {"ASL", "1000000100"},
+    {"ASR", "1000000101"},
+    {"LSR", "1000000110"},
+    {"ROL", "1000000111"},
+    {"ROR", "1000001000"},
+    {"RLC", "1000001001"},
+    {"RRC", "1000001010"},
+    {"JMP", "1000001011"},
+    {"CALL", "1000001100"},
+    {"PUSH", "1000001101"},
+    {"POP", "1000001010"},
+    {"BR", "11000000"},
+    {"BNE", "11000001"},
+    {"BEQ", "11000010"},
+    {"BPL", "11000011"},
+    {"BMI", "11000100"},
+    {"BCS", "11000101"},
+    {"BCC", "11000110"},
+    {"BVS", "11000111"},
+    {"BVC", "11001000"},
+    {"CLC", "1110000000000000"},
+    {"CLV", "1110000000000001"},
+    {"CLZ", "1110000000000010"},
+    {"CLS", "1110000000000011"},
+    {"CCC", "1110000000000111"},
+    {"SEC", "1110000000010000"},
+    {"SEV", "1110000000010001"},
+    {"SEZ", "1110000000010010"},
+    {"SES", "1110000000010011"},
+    {"SCC", "1110000000011000"},
+    {"NOP", "1110000000011001"},
+    {"RET", "1110000000011010"},
+    {"RETI", "1110000000011011"},
+    {"HALT", "1110000000100000"},
+    {"WAIT", "1110000000100001"},
+    {"PUSHPC", "1110000000100010"},
+    {"POPPC", "1110000000100011"},
+    {"PUSHFLAG", "1110000000100100"},
+    {"POPFLAG", "1110000000100101"}
+};
         private String filename;
 
         public AssemblerMainForm()
@@ -64,12 +126,11 @@ namespace Assembler
 
         private void ParseFileButton_Click(object sender, EventArgs e)
         {
+            List<String> asmElements = new List<String>(); // Move this line outside the try block
             try
             {
                 /* local variable used for debugging only */
                 int lineCounter = 0;
-                /* List which will store each token (element) read from ASM file */
-                List<String> asmElements = new List<String>();
                 /* Create a parser object used for ASM file
                     REMEMBER: this parser can be used for all kind of text files!!!
                  */
@@ -77,7 +138,7 @@ namespace Assembler
                 /* Reinitialize the Text property of OutputTextBox */
                 OutputTextBox.Text = "";
                 /* Define delimiters in ASM file */
-                String[] delimiters = {":",","," ","(",")"};
+                String[] delimiters = { ":", ",", " ", "(", ")" };
                 /* Specify that the elements in ASM file are delimited by some characters */
                 parser.TextFieldType = FieldType.Delimited;
                 /* Set-up the specified delimiters */
@@ -126,7 +187,58 @@ namespace Assembler
             {
                 MessageBox.Show(exc.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+            GenerateObjectFile(asmElements, ASMFileTextBox.Text);
         }
+
+        private void GenerateObjectFile(List<string> tokens, string asmPath)
+        {
+            try
+            {
+                // Crează folderul special pentru .obj
+                string objFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "GeneratedOBJ");
+                Directory.CreateDirectory(objFolder);
+
+                // Nume fișier .obj
+                string fileNameWithoutExt = Path.GetFileNameWithoutExtension(asmPath);
+                string objPath = Path.Combine(objFolder, fileNameWithoutExt + ".obj");
+
+                // Curăță fereastra UI
+                textBox1.Text = "";
+
+                using (StreamWriter sw = new StreamWriter(objPath))
+                {
+                    for (int i = 0; i < tokens.Count; i++)
+                    {
+                        string token = tokens[i].ToUpper();
+
+                        if (opcodeTable.ContainsKey(token))
+                        {
+                            string line = opcodeTable[token];
+                            int j = i + 1;
+
+                            while (j < tokens.Count && !opcodeTable.ContainsKey(tokens[j].ToUpper()))
+                            {
+                                line += " " + tokens[j];
+                                j++;
+                            }
+
+                            sw.WriteLine(line);                // Scrie în fișier
+                            textBox1.Text += line + "\r\n"; // Afișează în UI
+                        }
+                    }
+                }
+
+                MessageBox.Show("Fișierul .obj a fost salvat în:\n" + objPath,
+                    "Succes", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eroare la generarea fișierului .obj:\n" + ex.Message,
+                    "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
 
         private void OpenFileButton_Click(object sender, EventArgs e)
         {
@@ -154,6 +266,16 @@ namespace Assembler
             {
                 MessageBox.Show(exc.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        private void AssemblerMainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
